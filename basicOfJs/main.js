8b4c4b27b907aebe8b24213fb9c312a9af599984
Rupesh Pal <rupeshpal49@gmail.com>
2024-09-30 20:35:17 +0530

    add operators code in practice

--- basicOfJs/main.js
+++ basicOfJs/main.js
@@ -1,7 +1,44 @@
 let rollNumber = 20;
-console.log(rollNumber);
+// console.log(rollNumber);
 
-rollNumber = 21;
-console.log(rollNumber);
+rollNumber = 21; // we can reassign the value in the variable
+// console.log(rollNumber);
 
 // let rollNumber = 20; // We can not redeclare variable
+
+// Operators
+/*
++ Addition
+- Substractio
+* Multiplication
+/ Division
+% Modulus
+**  Power calc
+ */
+
+// 1. Addition  //
+function calculateNumbers(numVal1, numVal2) {
+  return numVal1 + numVal2;
+}
+
+console.log(calculateNumbers(10, 20));
+
+// 2. Substraction  //
+let valOne = 10;
+valTwo = 5;
+console.log(valOne - valTwo);
+
+// 3. Multiplication  //
+let mangoPrice = 120;
+let purchaseInKG = 5;
+console.log(mangoPrice * purchaseInKG);
+
+// 4. Division  //
+
+console.log(valOne / valTwo);
+
+// 5. Modulus  //
+let num1 = 19;
+let num2 = 7;
+
+console.log(num1 % num2);
