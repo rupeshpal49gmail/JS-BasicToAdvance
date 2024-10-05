2a7d67009554e4ea66b000c9a090644a59a7433b
Rupesh Pal <rupeshpal49@gmail.com>
2024-10-06 01:51:21 +0530

    add conditional statement

--- basicOfJs/main.js
+++ basicOfJs/main.js
@@ -1,8 +1,8 @@
 let rollNumber1 = 20;
-console.log(rollNumber1);
+// console.log(rollNumber1);
 
 rollNumber1 = 21; // we can reassign the value in the variable
-console.log(rollNumber1);
+// console.log(rollNumber1);
 
 // let rollNumber1 = 20; // We can not redeclare variable
 
@@ -21,50 +21,50 @@ function calculateNumbers(numVal1, numVal2) {
   return numVal1 + numVal2;
 }
 
-console.log(calculateNumbers(10, 20));
+// console.log(calculateNumbers(10, 20));
 
 // 2. Substraction  //
 let valOne = 10;
 valTwo = 5;
-console.log(valOne - valTwo);
+// console.log(valOne - valTwo);
 
 // 3. Multiplication  //
 let mangoPrice = 120;
 let purchaseInKG = 5;
-console.log(mangoPrice * purchaseInKG);
+// console.log(mangoPrice * purchaseInKG);
 
 // 4. Division  //
 
-console.log(valOne / valTwo);
+// console.log(valOne / valTwo);
 
 // 5. Modulus  //
 let num1 = 19;
 let num2 = 7;
 
-console.log(num1 % num2);
+// console.log(num1 % num2);
 
 // 6. Power  //
 let cuboidOfNumber = 4;
-console.log(cuboidOfNumber ** 2);
+// console.log(cuboidOfNumber ** 2);
 
 // Increament, Decrement and Operator //
 let a = 10;
-console.log(a);
-console.log(a++);
-console.log(a--);
-console.log(a);
-console.log(++a);
-console.log(a);
-console.log(--a);
-console.log(a);
+// console.log(a);
+// console.log(a++);
+// console.log(a--);
+// console.log(a);
+// console.log(++a);
+// console.log(a);
+// console.log(--a);
+// console.log(a);
 
 // Assignment Operator  //
 
 let numbers1 = 10;
-console.log(numbers1);
+// console.log(numbers1);
 
 numbers1 += 3;
-console.log(`Here assigned number is ${numbers1}`);
+// console.log(`Here assigned number is ${numbers1}`);
 
 // Comparision operators //
 /* > 
@@ -77,19 +77,19 @@ console.log(`Here assigned number is ${numbers1}`);
 !==
 */
 
-console.log(10 > 9);
-console.log(10 < 9);
-console.log(10 >= 9);
-console.log(10 <= 9);
-console.log(10 == 9);
-console.log(10 != 9);
-console.log(10 === 9);
-console.log(10 !== 9);
-console.log(
-  `Here is loose strict typ of 10 == "10" will be: ${
-    10 == "10"
-  } because == operator check only value and === operator check value and type both`
-);
+// console.log(10 > 9);
+// console.log(10 < 9);
+// console.log(10 >= 9);
+// console.log(10 <= 9);
+// console.log(10 == 9);
+// console.log(10 != 9);
+// console.log(10 === 9);
+// console.log(10 !== 9);
+// console.log(
+//   `Here is loose strict typ of 10 == "10" will be: ${
+//     10 == "10"
+//   } because == operator check only value and === operator check value and type both`
+// );
 // s
 
 // Logical Operator  //
@@ -98,18 +98,50 @@ console.log(
 
 let age = 20;
 let status1 = age > 18 ? "I can vote" : "I can not vote";
-console.log(status1);
+// console.log(status1);
 
 let result = true && false && true;
-console.log(`The output is ${result}`);
+// console.log(`The output is ${result}`);
 
 let res2 = false || 7 || 10 || 18;
-console.log(res2);
+// console.log(res2);
 
 let res3 = true && false;
-console.log(res3);
+// console.log(res3);
 
 let res4 = true && false && true;
-console.log(res4);
+// console.log(res4);
 
 let res5 = true || false || true;
+// console.log(res5);
+
+// Bitwise operators  //
+// AND(&), OR(||), Not(!, ~), <<, >> XOR(^) //
+
+// console.log(2 ^ 2);
+
+// Conditionals //
+
+let studentsAge = 10;
+// 1. if statement //
+if (studentsAge >= 18) {
+  console.log("Students can participate in election");
+}
+
+// 2. if else statement //
+if (studentsAge >= 20) {
+  console.log("He is eligible for participate in stunt mania training");
+} else {
+  console.log(
+    "He needs to take certificate from RTO to get eligibility critera"
+  );
+}
+
+// 3 if else if statement //
+if (studentsAge >= 30) {
+  console.log("RTO certificate and stunt mania cetificate both are mandatory");
+} else if (studentsAge >= 35) {
+  console.log("He must have professional certificae");
+} else {
+  console.log("He must have to pass all the criteria");
+}
