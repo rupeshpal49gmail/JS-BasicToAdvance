a6371cfa5335bb25b3dc7e5881f0a614df8d9f60
Rupesh Pal <rupeshpal49@gmail.com>
2024-10-19 00:47:50 +0530

    add numbers and math

--- basicOfJs/script.js
+++ basicOfJs/script.js
@@ -2,6 +2,7 @@
 // Strings  //
 const userName = "My Name is John Doe";
 const userEmail = "johndoe39@gmail.com";
+console.log(userName.__proto__);
 
 let result = "My name is " + userName + " and my user name is " + userEmail;
 console.log(result);
@@ -35,3 +36,39 @@ console.log(url.replace("%30", "/"));
 console.log(url.includes("you"));
 
 console.log(url.includes("with me"));
+
+// *** Number *** //
+
+const balance = 1000;
+console.log(balance);
+console.log(typeof balance);
+// Let's define variable with new keyword //
+const score = new Number(100);
+console.log(score);
+
+console.log(score.toString());
+console.log(score.toString().length);
+console.log(score.toFixed(2));
+
+const newNumbers = 19230.343434;
+console.log(newNumbers.toPrecision(3));
+
+const numValue = 10000200;
+console.log(numValue.toLocaleString("en-IN"));
+
+// *** Maths  **** //
+console.log(Math);
+console.log(Math.abs(-30));
+console.log(Math.round(20.34));
+console.log(Math.ceil(34.23));
+console.log(Math.floor(43.4444));
+console.log(Math.min(1, 234, 43, 4, 23, 23, 4, 2, 2, 2));
+console.log(Math.max(1, 234, 43, 4, 23, 23, 4, 2, 2, 2));
+console.log(Math.random());
+console.log(Math.random() * 10);
+console.log(Math.random() * 10 + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
