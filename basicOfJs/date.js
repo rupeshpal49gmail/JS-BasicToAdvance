a9b1fd4ff92accd2891523fecd3feaca2776f087
Rupesh Pal <rupeshpal49@gmail.com>
2024-10-20 02:16:36 +0530

    add date and time

---
+++ basicOfJs/date.js
@@ -0,0 +1,17 @@
+let getDay = new Date();
+// console.log(getDay);
+// console.log(getDay.toString());
+// console.log(getDay.toDateString());
+// console.log(getDay.toLocaleString());
+// console.log(getDay.getTime());
+// console.log(getDay.getMonth());
+// console.log(typeof getDay);
+
+let createYourOwnDate = new Date(2024, 4, 20);
+console.log(createYourOwnDate);
+console.log(createYourOwnDate.toDateString());
+console.log(createYourOwnDate.toLocaleString());
+
+let timeStamp = Date.now();
+console.log(timeStamp);
+console.log(createYourOwnDate.getTime());
