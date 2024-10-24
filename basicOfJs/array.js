67a31e223c19b333df92a156e4b3217780cb2531
Rupesh Pal <rupeshpal49@gmail.com>
2024-10-24 23:57:18 +0530

    add array continue

--- basicOfJs/array.js
+++ basicOfJs/array.js
@@ -7,22 +7,50 @@
 // How to decalare array //
 // let newArray = [1,2,3,4,"One", "Two", "Three", true]; //
 
-let numArray = [1, 2, 3, 4, 5];
-console.log(numArray);
-numArray.push(100);
-console.log(numArray);
-numArray.pop();
-console.log(numArray);
-numArray.shift();
-console.log(numArray);
-numArray.unshift(90);
-console.log(numArray);
-console.log(numArray.includes(4));
-console.log(numArray.indexOf(5));
-console.log(numArray.at(2));
+// let numArray = [1, 2, 3, 4, 5];
+// console.log(numArray);
+// numArray.push(100);
+// console.log(numArray);
+// numArray.pop();
+// console.log(numArray);
+// numArray.shift();
+// console.log(numArray);
+// numArray.unshift(90);
+// console.log(numArray);
+// console.log(numArray.includes(4));
+// console.log(numArray.indexOf(5));
+// console.log(numArray.at(2));
 
 // Slice and Splice //
 
-let rollNumber = [1, 2, 3, 4, 5, 6];
-console.log(rollNumber.slice(1, 2));
-console.log(rollNumber.splice(1, 2));
+// let rollNumber = [1, 2, 3, 4, 5, 6];
+// console.log(rollNumber.slice(1, 2));
+// console.log(rollNumber.splice(1, 2));
+
+const carsBrand = ["Tata", "Mahindra", "Maruti"];
+const bikeBrand = ["Hero", "TVS", "Bajaj", "Royal Enfield"];
+
+// 1. ** By Push method  **  //
+// carsBrand.push(bikeBrand);  // This is not a recommended way //
+// console.log(carsBrand);
+
+// ** By concat method ** //
+// let outputRes = carsBrand.concat(bikeBrand);  // This one is okay method //
+// console.log(outputRes);
+
+// ** By spread operator method ** //
+const allBrands = [...carsBrand, ...bikeBrand];
+console.log(allBrands);
+
+const compBrand = [
+  "HCL",
+  "Wipro",
+  "Dell",
+  ["Samsung", "HP"],
+  [1, 2, 3, [4, 5, 6, [7, 8, 9]]],
+];
+console.log(compBrand);
+
+const finalBrand = compBrand.flat(Infinity);
+
+console.log(finalBrand);
