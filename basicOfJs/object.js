35ab75749ff5b6d42d5b59fd7164e992e7583b1a
Rupesh Pal <rupeshpal49@gmail.com>
2024-11-10 23:59:53 +0530

    add constructor methods of object

--- basicOfJs/object.js
+++ basicOfJs/object.js
@@ -10,18 +10,27 @@
 // console.log(stdName.fName);
 // console.log(stdName["lName"]);  // recomonded method  //
 
-const mySym = Symbol("key1");
+// const mySym = Symbol("key1");
 
-const heyBuddy = {
-  name: "Rupesh",
-  [mySym]: "mykey1",
-  age: 29,
-  location: "Surat",
-};
+// const heyBuddy = {
+//   name: "Rupesh",
+//   [mySym]: "mykey1",
+//   age: 29,
+//   location: "Surat",
+// };
 
-console.log(heyBuddy[mySym]); // While working with symbol object need to follow given methods //
+// console.log(heyBuddy[mySym]); // While working with symbol object need to follow given methods //
 
-heyBuddy.age = 30;
-Object.freeze(heyBuddy);
-heyBuddy.age = 40;
-console.log(heyBuddy + "I am not able to change existing data");
+// heyBuddy.age = 30;
+// Object.freeze(heyBuddy);
+// heyBuddy.age = 40;
+// console.log(heyBuddy + "I am not able to change existing data");
+
+// lets play with constructor methods  //
+
+const instaUser = new Object((userName = "Rupesh"), (age = 29));
+console.log(instaUser);
+console.log(instaUser["userName"]);
+console.log(instaUser[userName]);
+
+//
