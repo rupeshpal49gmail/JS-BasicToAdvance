87259a4954e2c09a2b423321a7075ae1d00716d9
Rupesh Pal <rupeshpal49@gmail.com>
2024-11-13 00:00:09 +0530

    add function and parameters

---
+++ basicOfJs/funcPara.js
@@ -0,0 +1,14 @@
+// Function  //
+// function addTwoNumbers(num1, num2) {
+//   console.log(num1 * num2);
+// }
+
+// addTwoNumbers(3, 4);
+// let result = addTwoNumbers(2, 4);
+// console.log(`Result : ${result}`);
+
+function addTwoNumbers(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(addTwoNumbers(3, 4));
