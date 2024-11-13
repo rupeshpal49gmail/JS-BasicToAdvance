9ab80638ede2e03439b038cbc0e43894983715ec
Rupesh Pal <rupeshpal49@gmail.com>
2024-11-13 23:57:35 +0530

    add arrow function

--- basicOfJs/arrowFunc.js
+++ basicOfJs/arrowFunc.js
@@ -1,18 +1,49 @@
 // 1. This keyword //
 
-const user = {
-  username: "John",
-  age: 29,
-  welcomeMessage: function () {
-    console.log(`Hey ${this.username}, welcome to home`);
-    console.log(this);
-  },
-};
-user.welcomeMessage();
-user.username = "Ryan Dhull";
-user.welcomeMessage();
+// const user = {
+//   username: "John",
+//   age: 29,
+//   welcomeMessage: function () {
+//     console.log(`Hey ${this.username}, welcome to home`);
+//     console.log(this);
+//   },
+// };
+// user.welcomeMessage();
+// user.username = "Ryan Dhull";
+// user.welcomeMessage();
 
-function newOne() {
-  console.log(this);
-}
-newOne();
+// function newOne() {
+//   console.log(this);
+// }
+// newOne();
+
+// 2. Arrow Function  //
+
+const arrFunc = function () {
+  let username = "John";
+  console.log(username);
+};
+arrFunc();
+
+const arrowFunction = () => {
+  let newName = "New John";
+  console.log(newName);
+};
+arrowFunction();
+
+// const addTwoNum = (num1, num2) => {
+//   let output = num1 + num2;
+//   console.log(output);
+// };
+// addTwoNum(3, 8);
+
+// const addTwoNum = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// console.log(addTwoNum(3, 8));
+
+// *** Implicit Return *** //
+
+const addNumValue = (number1, number2) => number1 + number2;
+console.log(addNumValue(23, 32));
