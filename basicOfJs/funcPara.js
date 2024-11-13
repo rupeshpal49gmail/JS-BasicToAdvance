08137f5f48ddeac485b621ae11c1b53186dd520e
Rupesh Pal <rupeshpal49@gmail.com>
2024-11-13 23:05:55 +0530

    add scope level and mini hoisting

--- basicOfJs/funcPara.js
+++ basicOfJs/funcPara.js
@@ -35,7 +35,54 @@
 // }
 // console.log(myNumbers(100, 200, 300, 400));
 
-function myNumbers(memory1, memory2, ...numers1) {
-  return numers1;
+// function myNumbers(memory1, memory2, ...numers1) {
+//   return numers1;
+// }
+// console.log(myNumbers(100, 200, 300, 400, 789));
+
+// *** Scope *** //
+function heyOne() {
+  const username = "John Doe";
+  function heyTwo() {
+    const website = "My name is John Doe";
+    console.log(username);
+  }
+  //   console.log(website);  // output is error due to outside of the scope
+  heyTwo();
 }
-console.log(myNumbers(100, 200, 300, 400, 789));
+heyOne();
+
+if (true) {
+  const stdName = "John Doe Newton";
+  if (stdName === "John Doe Newton") {
+    const webpage = "Practice paper";
+    console.log(stdName + webpage);
+    console.log(webpage);
+  }
+  //   console.log(webpage); // Output error due to outside of scope
+  console.log(stdName);
+}
+
+// console.log(stdName); // Output error due to outside of scope
+
+// function addOne(num1) {
+//   return num1 + 5;
+// }
+// console.log(addOne(5))
+
+console.log(addOne(5)); // output: 10
+function addOne(num1) {
+  return num1 + 5;
+}
+
+const addTow = function (num2) {
+  return num2 + 2;
+};
+
+console.log(addTow(5));
+
+// console.log(addTow(5)); // Output: error :- not accessable
+
+// const addTow = function (num2) {
+//   return num2 + 2;
+// };
