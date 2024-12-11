17ac77ba2c724f76e55af1a8c788819dae5a3301
Rupesh Pal <rupeshpal49@gmail.com>
2024-12-11 23:52:45 +0530

    add loop by es6

--- basicOfJs/loops.js
+++ basicOfJs/loops.js
@@ -1,6 +1,6 @@
 // Loops or Iteration //
 /// 1. for loop  //
-
+/*
 for (let i = 0; i < 10; i++) {
   console.log(`The value of ${i} : ${i}`);
 }
@@ -48,3 +48,68 @@ do {
   console.log(`Score is ${score}`);
   score++;
 } while (score <= 5);
+*/
+
+// for of loop and for in loop //
+const numberArr = [1, 2, 3, 4, 5];
+
+for (const numbers of numberArr) {
+  // console.log(numbers);
+}
+
+const sayHi = "Hi Devin!";
+for (const greet of sayHi) {
+  // Here with continue statement I can ignore space between words //
+  if (greet == " ") {
+    continue;
+  }
+  // console.log(`Each char is: ${greet}`);
+}
+
+// Maps  //
+const mapValue = new Map();
+mapValue.set("IN", "India");
+mapValue.set("USA", "United State of America");
+mapValue.set("Gr", "Germany");
+mapValue.set("IN", "India");
+// console.log(mapValue);
+
+for (const [key, value] of mapValue) {
+  // console.log(`${key} :-  ${value}`);
+}
+
+const myGames = {
+  game1: "NFS",
+  game2: "Counter Strike",
+  game3: "GTA",
+};
+
+// for of loop is not iterable on  object  //
+// for (const [key, value] of myGames) {
+//   console.log(key, ":-", value);
+// }
+
+// *********** //
+const myObj = {
+  js: "Javascript",
+  cpp: "C++",
+  rv: "Ruby",
+};
+// for object we use for in loop to iterate //
+
+for (const obj in myObj) {
+  // console.log(`${obj} Stand for ${myObj[obj]}`);
+}
+
+const fruitsName = ["mango", "Banana", "Apple"];
+
+for (const fruits in fruitsName) {
+  // console.log(`${fruitsName[fruits]}`);
+}
+
+// forEach loop //
+const prLang = ["c", "c++", "js", "java"];
+
+prLang.forEach((item) => {
+  console.log(item);
+});
