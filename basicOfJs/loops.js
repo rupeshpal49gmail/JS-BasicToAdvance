4e4a45029eb32c19ebf27bfe79c5c05db825a892
Rupesh Pal <rupeshpal49@gmail.com>
2024-12-13 00:32:36 +0530

    Add reduce

--- basicOfJs/loops.js
+++ basicOfJs/loops.js
@@ -166,8 +166,27 @@ const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // console.log(newNum);
 
-const newNums = myNums.map((num) => {
-  return num + 10;
-});
+// const newNums = myNums.map((num) => {
+//   return num + 10;
+// });
 
-console.log(newNums);
+// console.log(newNums);
+
+// *** Chaining methods *** //
+
+// const newNum = myNums
+//   .map((num) => num * 10)
+//   .map((num) => num + 1)
+//   .filter((num) => num >= 20);
+// console.log(newNum);
+
+// *** Reduce *** //
+const number = [1, 2, 3, 4];
+
+const finalRes = number.reduce((acc, currVal) => {
+  console.log(`acc: ${acc} and currVal: ${currVal}`);
+
+  return acc + currVal;
+}, 0);
+
+console.log(finalRes);
