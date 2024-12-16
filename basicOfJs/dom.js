057616fa852bae3875e46d846878fd33abf557a6
Rupesh Pal <rupeshpal49@gmail.com>
2024-12-16 23:59:50 +0530

    add dome basics

--- basicOfJs/dom.js
+++ basicOfJs/dom.js
@@ -1,3 +1,18 @@
-document.getElementById("heading").addEventListener("click", function (e) {
-  console.log(e);
-});
+const parent = document.querySelector(".parent");
+// console.log(parent);
+// console.log(parent.children);
+// console.log(parent.children[1]);
+// console.log(parent.children[1].innerHTML);
+
+for (let i = 0; i < parent.children.length; i++) {
+  // console.log(parent.children[i].innerHTML);
+}
+parent.children[1].style.color = "red";
+console.log(parent.firstElementChild);
+console.log(parent.lastElementChild);
+
+const dayOne = document.querySelector(".day");
+console.log(dayOne);
+console.log(dayOne.parentElement);
+console.log(dayOne.nextElementSibling);
+console.log(dayOne.nextSibling);
