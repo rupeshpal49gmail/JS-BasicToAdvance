329303070231132f096d5310338d09708f797558
Rupesh Pal <rupeshpal49@gmail.com>
2024-12-27 00:32:35 +0530

    add dom manipulation

--- basicOfJs/dom.js
+++ basicOfJs/dom.js
@@ -19,17 +19,60 @@
 
 // Create Element in DOM  //
 
-const div = document.createElement("div");
+// const div = document.createElement("div");
 
-console.log(div);
-div.className = "parnetNode";
-div.id = "myId";
+// console.log(div);
+// div.className = "parnetNode";
+// div.id = "myId";
 
-console.log(div);
-div.id = Math.round(Math.random() * 10 + 1);
-div.setAttribute("title", "Title created");
-div.style.backgroundColor = "purple";
-div.style.padding = "20px";
-const addText = document.createTextNode("Hello Dom");
-div.appendChild(addText);
-document.body.appendChild(div);
+// console.log(div);
+// div.id = Math.round(Math.random() * 10 + 1);
+// div.setAttribute("title", "Title created");
+// div.style.backgroundColor = "purple";
+// div.style.padding = "20px";
+// const addText = document.createTextNode("Hello Dom");
+// div.appendChild(addText);
+// document.body.appendChild(div);
+
+// Create element in DOM with function  //
+
+// function addNewLang(langauge) {
+//   const li = document.createElement("li");
+//   li.innerHTML = `${langauge}`;
+//   document.querySelector(".language").appendChild(li);
+// }
+// addNewLang("Python");
+// addNewLang("Java");
+// addNewLang("C++");
+// addNewLang("C#");
+
+function addotherMethod(langauge) {
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(langauge));
+  document.querySelector(".language").appendChild(li);
+}
+addotherMethod("Ruby");
+addotherMethod("C#");
+addotherMethod(".Net");
+
+// Edit Element in DOM  //
+
+const secLang = document.querySelector("li:nth-child(2)");
+// secLang.innerHTML = "golang";
+
+// other approach to edit element in DOM  //
+const newLi = document.createElement("li");
+newLi.textContent = "Node.js";
+secLang.replaceWith(newLi);
+
+// Edit Element in DOM  //
+// const fLang = document.querySelector("li:first-child");
+// const fLang = document.querySelector("li:last-child");
+const fLang = document.querySelector("li:nth-child(2)");
+fLang.outerHTML = "<li>React</li>";
+
+// Remove Element in DOM  //
+// const lastLang = document.querySelector("li:first-child");
+// const lastLang = document.querySelector("li:last-child");
+const lastLang = document.querySelector("li:nth-child(2)");
+lastLang.remove();
