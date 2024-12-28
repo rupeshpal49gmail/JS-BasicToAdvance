5493230948054257618280038d5f58f8374d7fc0
Rupesh Pal <rupeshpal49@gmail.com>
2024-12-29 00:32:02 +0530

    Add styling to the form

--- basicOfJs/project2.js
+++ basicOfJs/project2.js
@@ -4,12 +4,17 @@ console.log(formData);
 formData.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  const height = document.querySelector("#height").value;
-  const weight = document.querySelector("#weight").value;
+  const height = parseInt(document.querySelector("#height").value);
+  const weight = parseInt(document.querySelector("#weight").value);
   const results = document.querySelector("#results");
 
   if (height === "" || height < 0 || isNaN(height)) {
-    results.innerHTML = `Please give a vald point ${height}`;
+    results.innerHTML = `Please give a valid entry ${height}`;
+  } else if (weight === "" || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please give a vald point ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    //Show the result
+    results.innerHTML = `<span>${bmi}</span>`;
   }
-  results.innerHTML = `${height}`;
 });
